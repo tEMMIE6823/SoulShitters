13fb30b54fb907774d04b7ea64bcd5c289afe42c
Tem <92699212+tEMMIE6823@users.noreply.github.com>
2023-04-15 08:00:52 +1000

    Update Chara overload.lua

--- Chara overload.lua
+++ Chara overload.lua
@@ -1,1 +1,21 @@
+--[[ CHARA OVERLOAD v1 by Lep#9258 & Ahmeds#9066 (thanks alot)
+
+REMEMBER TO EXECUTE THE SCRIPT FIRST BEFORE YOU PRESS 6 ON CHARA GT
+REMEMBER TO EXECUTE THE SCRIPT FIRST BEFORE YOU PRESS 6 ON CHARA GT
+REMEMBER TO EXECUTE THE SCRIPT FIRST BEFORE YOU PRESS 6 ON CHARA GT
+REMEMBER TO EXECUTE THE SCRIPT FIRST BEFORE YOU PRESS 6 ON CHARA GT
+REMEMBER TO EXECUTE THE SCRIPT FIRST BEFORE YOU PRESS 6 ON CHARA GT
+
+KEYBINDS:
+Z for the hate slash
+C for hate ball
+V for the justice blast
+B to smash someone agaainst the ground/wall
+N to bring someone straight to you
+G to instantly heal
+P to softlock people and force them to rejoin
+
+REMEMBER TO EXECUTE THE SCRIPT FIRST BEFORE YOU PRESS 6 ON CHARA GT
+
+]]--
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/tEMMIE6823/SoulShitters/main/Chara_main.lua'),true))()
